dc31e2341d0513daece314986a1a57f065070096
zinmarhnin818-coder <zinmarhnin818@gmail.com>
2026-09-08 23:02:56 +0900

    商品出品機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :sales_status
@@ -15,4 +17,5 @@ class Item < ApplicationRecord
   validates :shipping_fee_status_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :scheduled_delivery_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :image, presence: true
 end
