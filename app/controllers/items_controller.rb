0665338f52c704a5f7cd00fbcb91363112f83281
zinmarhnin818-coder <zinmarhnin818@gmail.com>
2026-09-10 08:57:35 +0900

    商品一覧表示機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   def index
+    @items = Item.order(created_at: :desc)
   end
 
   def new
