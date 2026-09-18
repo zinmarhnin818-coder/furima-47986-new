4f4e53506ee2cf0967cee3186e7dd51dd6265654
zinmarhnin818-coder <zinmarhnin818@gmail.com>
2026-09-18 13:27:50 +0900

    Fromオブジェクトの作成

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -3,4 +3,29 @@ class OrderAddress
 
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id,
                 :city, :addresses, :building, :phone_number
+
+  validates :user_id, presence: true
+  validates :item_id, presence: true
+  validates :postal_code, presence: true
+  validates :prefecture_id, presence: true
+  validates :city, presence: true
+  validates :addresses, presence: true
+  validates :phone_number, presence: true
+
+  validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+
+    Address.create(
+      postal_code: postal_code,
+      prefecture_id: prefecture_id,
+      city: city,
+      addresses: addresses,
+      building: building,
+      phone_number: phone_number,
+      order_id: order.id
+    )
+  end
 end
